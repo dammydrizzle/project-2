9fe9f46072d5f58f47c0dca144bdd8a488c7fc48
Adedokun Ishaq Damilola <dammydrizzle@gmail.com>
2023-08-08 17:18:32 +0100

    added another variable totalSpan

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const btnElement = document.getElementById("calculate")
 const billInput = document.getElementById("bill")
 const tipInput = document.getElementById("tip")
-
+const totalSpan = document.getElementById("total")
 
 function calculateTotal() {
-    
+    const billValue = billInput.Value
+    const tipValue = tipInput.Value
+    const totalValue = billValue *(1 + tipValue / 100)
+
+
 }
 
 
