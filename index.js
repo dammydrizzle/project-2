4644e8dda72a2270dab251356203af9208f77622
Adedokun Ishaq Damilola <dammydrizzle@gmail.com>
2023-08-08 17:03:19 +0100

    created a function calculateTotal

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const btnElement = document.getElementById("calculate")
 
 
 
+function calculateTotal() {
+    console.log("clicked")
+}
+
+
+
+
+
+
 btnElement.addEventListener('click', calculateTotal)
